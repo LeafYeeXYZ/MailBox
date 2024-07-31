9d68c5cd32e2d59d61d71d60fc713564daafbd8a
LeafYeeXYZ <leafyeexyz@outlook.com>
2024-07-31 09:53:33 +0800

    init

---
+++ app/layout.tsx
@@ -0,0 +1,39 @@
+import type { Metadata, Viewport } from 'next'
+import './tailwind.css'
+import { ConfigProvider } from 'antd'
+
+export const metadata: Metadata = {
+  title: 'Mail Box',
+}
+
+export const viewport: Viewport = {
+  width: 'device-width',
+  initialScale: 1,
+  minimumScale: 1,
+  maximumScale: 1,
+  userScalable: false,
+}
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode
+}>) {
+
+  return (
+    <html lang="zh-CN">
+      <body className='overflow-hidden'>
+        <ConfigProvider 
+          theme={{
+            token: {
+              colorPrimary: '#ff8080',
+              colorText: '#4c0519'
+            },
+          }}
+        >
+          {children}
+        </ConfigProvider>
+      </body>
+    </html>
+  )
+}
