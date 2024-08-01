bcdde402fab7a2a21109a1c5ca1901f667c83b8a
LeafYeeXYZ <leafyeexyz@outlook.com>
2024-08-01 18:44:46 +0800

    update

--- app/(dashboard)/components/Nav.tsx
+++ app/(dashboard)/components/Nav.tsx
@@ -13,11 +13,11 @@ export function Nav() {
       style={{ scrollbarWidth: 'none' }} 
       className='relative flex flex-row sm:flex-col gap-2 sm:gap-3 w-full h-full overflow-y-auto'
     >
-      <Link className={`bg-white text-nowrap shadow-sm w-full border text-xs sm:text-sm p-2 text-center rounded-full ${pathname === '/inbox' ? 'text-rose-400' : 'text-gray-500'}`} href='/inbox'><AppstoreOutlined /> 收件箱</Link>
-      <Link className={`bg-white text-nowrap shadow-sm w-full border text-xs sm:text-sm p-2 text-center rounded-full ${pathname === '/send' ? 'text-rose-400' : 'text-gray-500'}`} href='/send'><FileAddOutlined /> 新邮件</Link>
-      <Link className={`bg-white text-nowrap shadow-sm w-full border text-xs sm:text-sm p-2 text-center rounded-full ${pathname === '/sent' ? 'text-rose-400' : 'text-gray-500'}`} href='/sent'><FileDoneOutlined /> 已发邮件</Link>
-      <Link className={`bg-white text-nowrap shadow-sm w-full border text-xs sm:text-sm p-2 text-center rounded-full sm:absolute sm:bottom-[3.2rem] ${pathname === '/profile' ? 'text-rose-400' : 'text-gray-500'}`} href='/profile'><IdcardOutlined /> 个人信息</Link>
-      <a className='bg-white text-nowrap shadow-sm w-full border text-xs sm:text-sm p-2 text-center rounded-full sm:absolute sm:bottom-0 text-gray-500' href='#'
+      <Link className={`bg-white text-nowrap shadow-sm w-32 sm:w-full border text-xs sm:text-sm p-2 text-center rounded-full ${pathname === '/inbox' ? 'text-rose-400' : 'text-gray-500'}`} href='/inbox'><AppstoreOutlined /> 收件箱</Link>
+      <Link className={`bg-white text-nowrap shadow-sm w-32 sm:w-full border text-xs sm:text-sm p-2 text-center rounded-full ${pathname === '/send' ? 'text-rose-400' : 'text-gray-500'}`} href='/send'><FileAddOutlined /> 新邮件</Link>
+      <Link className={`bg-white text-nowrap shadow-sm w-32 sm:w-full border text-xs sm:text-sm p-2 text-center rounded-full ${pathname === '/sent' ? 'text-rose-400' : 'text-gray-500'}`} href='/sent'><FileDoneOutlined /> 已发邮件</Link>
+      <Link className={`bg-white text-nowrap shadow-sm w-32 sm:w-full border text-xs sm:text-sm p-2 text-center rounded-full sm:absolute sm:bottom-[3.2rem] ${pathname === '/profile' ? 'text-rose-400' : 'text-gray-500'}`} href='/profile'><IdcardOutlined /> 个人信息</Link>
+      <a className='bg-white text-nowrap shadow-sm w-32 sm:w-full border text-xs sm:text-sm p-2 text-center rounded-full sm:absolute sm:bottom-0 text-gray-500' href='#'
         onClick={event => {
           event.preventDefault()
           localStorage.clear()
