edce1a8a507c816b33af1a8af223bde3d07ccaa7
LeafYeeXYZ <leafyeexyz@outlook.com>
2024-08-01 18:24:54 +0800

    update

--- app/(dashboard)/components/Nav.tsx
+++ app/(dashboard)/components/Nav.tsx
@@ -11,7 +11,7 @@ export function Nav() {
   return (
     <div
       style={{ scrollbarWidth: 'none' }} 
-      className='relative flex flex-row sm:flex-col gap-2 sm:gap-3 w-full h-full overflow-y-auto'
+      className='relative flex flex-row sm:flex-col gap-2 sm:gap-3 w-full h-full overflow-y-scroll sm:overflow-y-hidden'
     >
       <Link className={`bg-white text-nowrap shadow-sm w-full border text-xs sm:text-sm p-2 text-center rounded-full ${pathname === '/inbox' ? 'text-rose-400' : 'text-gray-500'}`} href='/inbox'><AppstoreOutlined /> 收件箱</Link>
       <Link className={`bg-white text-nowrap shadow-sm w-full border text-xs sm:text-sm p-2 text-center rounded-full ${pathname === '/send' ? 'text-rose-400' : 'text-gray-500'}`} href='/send'><FileAddOutlined /> 新邮件</Link>
